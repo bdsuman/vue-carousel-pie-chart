3a61ad001c461a1323143fab06ff351ffd6a2fdc
Suman Sen <mesuman@yahoo.com>
2023-08-14 11:07:41 +0600

    carousel complated

--- src/components/Carousel.vue
+++ src/components/Carousel.vue
@@ -1,48 +1,65 @@
-<template>
-  <div class="carousel w-full">
-    <button @click="prevSlide" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-      Previous
-    </button>
-    <div class="slide-container">
-      <transition name="fade" mode="out-in">
-        <img :key="currentSlide" :src="images[currentSlide]" alt="Carousel Slide" class="w-full">
-      </transition>
-    </div>
-    <button @click="nextSlide" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-      Next
-    </button>
-  </div>
-</template>
-
 <script setup>
-import { ref, onMounted, onBeforeUnmount } from 'vue';
+import {ref, onMounted, nextTick, onBeforeUnmount} from 'vue';
+const items = ref([
+  'https://images.unsplash.com/photo-1494438639946-1ebd1d20bf85?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1467&q=80',
+  'https://images.unsplash.com/photo-1505691938895-1758d7feb511?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80',
+  'https://images.unsplash.com/photo-1558882224-dda166733046?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1469&q=80',
+  'https://images.unsplash.com/photo-1562664377-709f2c337eb2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80',
+  'https://images.unsplash.com/photo-1476514525535-07fb3b4ae5f1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80',
+  'https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1473&q=80',
+])
 
-const currentSlide = ref(0);
-const images = [
-  'https://images.unsplash.com/photo-1620765971982-c5c4e1d1bd75?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1100&q=80',
-  'https://images.unsplash.com/photo-1620765970846-3274335578d6?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1400&q=80',
-  'https://images.unsplash.com/photo-1460648304944-4883b5cfcee5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8c2xpZGVyfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60',
-    'https://plus.unsplash.com/premium_photo-1681488000659-cc08a1c6363e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjV8fHNsaWRlcnxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60',
-];
+let carousel = null
 
-const prevSlide = () => {
-  currentSlide.value = (currentSlide.value - 1 + images.length) % images.length;
-};
+const newItem = ref('https://images.unsplash.com/photo-1682685794761-c8e7b2347702?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2370&q=80')
 
-const nextSlide = () => {
-  currentSlide.value = (currentSlide.value + 1) % images.length;
-};
+function addNewitem() {
+  items.value.push(newItem.value)
+  carousel.destroy()
+  nextTick(function () {
+    carousel = new Flickity('#carousel',{
+      autoPlay: true,
+      wrapAround: true
+    })
+  })
+}
 
-let interval;
-onMounted(() => {
-  interval = setInterval(nextSlide, 5000); // Auto advance every 5 seconds
-});
-
+onMounted(()=>{
+  carousel = new Flickity('#carousel',{
+    autoPlay: true,
+    wrapAround: true
+  })
+  // interval = setInterval(nextSlide, 5000); // Auto advance every 5 seconds
+})
 onBeforeUnmount(() => {
-  clearInterval(interval);
+  carousel.destroy()
 });
 </script>
 
-<style scoped>
+<template>
 
+  <div class="my-10">
+    <input type="text" v-model="newItem">
+    <button @click="addNewitem()" class="ml-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+      Button
+    </button>
+
+  </div>
+  <div class="mx-auto items" id="carousel">
+    <div :style="`background-image:url(${item})`" class="item" v-for="(item, index) in items" :key="item">{{ index + 1 }}</div>
+  </div>
+</template>
+
+<style scoped>
+.items {
+  width: 600px;
+  height: 400px;
+}
+
+.item {
+  width: 600px;
+  height: 400px;
+  background-color: #ccc;
+  background-size: cover;
+}
 </style>
